3a680f59be9fb0cd5840fd8937b2aa4a947b5f1d
pomnom <yuslibra6@gmail.com>
2021-07-15 07:54:10 +0800

    make model

---
+++ app/Http/Controllers/FloraMController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FloraM;
+use Illuminate\Http\Request;
+
+class FloraMController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FloraM  $floraM
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FloraM $floraM)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FloraM  $floraM
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FloraM $floraM)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FloraM  $floraM
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FloraM $floraM)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FloraM  $floraM
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FloraM $floraM)
+    {
+        //
+    }
+}
---
+++ database/factories/FloraMFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\FloraM;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class FloraMFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = FloraM::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
